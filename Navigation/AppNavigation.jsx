b85f3e1c4ef6b8c641807e6a6d0d25ce770d6948
Caroline DebBaruah <cdebbaruah@gmail.com>
2023-11-26 15:13:22 -0800

    inital setup
    installed navigation, created basic structure for files

---
+++ Navigation/AppNavigation.jsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../Screens/HomeScreen';
+import ActivityHomeScreen from '../Screens/WritingActivities/ActivityHomeScreen';
+import StoryStarterScreen from '../Screens/StoryStarterScreen';
+import MindBodyScreen from '../Screens/MindBodyScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Activities" component={ActivityHomeScreen} />
+        <Tab.Screen name="Story Starters" component={StoryStarterScreen} />
+        <Tab.Screen name="Mind & Body" component={MindBodyScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+ }
---
+++ Screens/MindBodyScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function MindBodyScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Mind and Body Screen</Text>
+    </View>
+  );
+}
