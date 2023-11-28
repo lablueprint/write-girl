19c93099f38b2b1c9fc29eb1ea64e9e95730d781
ayubali24 <ayubali2443@gmail.com>
2023-11-27 22:46:27 -0800

    setup basic routes, controllers, models, and displayed the data to the screen

--- mobile/Screens/WritingActivities/ProgressiveWriting.jsx
+++ mobile/Screens/WritingActivities/ProgressiveWriting.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet, Text, View, Button,
+} from 'react-native';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,10 +13,49 @@ const styles = StyleSheet.create({
   },
 });
 
+const givenActivities = {
+  activity: 'baseball',
+  genre: 'sport',
+};
+
 export default function ProgressiveWritingScreen() {
+  const [activities, setActivities] = useState([]);
+
+  const getAllActivities = async () => {
+    const res = await axios.get(`${process.env.EXPO_PUBLIC_SERVER_URL}/activity/getAllActivities`);
+    setActivities(res.data);
+  };
+
+  useEffect(() => {
+    getAllActivities();
+  }, [activities]);
+
+  const addNewActivity = async (newActivity) => {
+    const res = await axios.post(`${process.env.EXPO_PUBLIC_SERVER_URL}/activity/createActivity`, newActivity);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Progressive Writing Screen</Text>
+      {
+        activities.map(({ activity, genre, _id }) => (
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>
+              Activity:
+              {' '}
+              {activity}
+              Genre:
+              {'  '}
+              {genre}
+              ID:
+              {'  '}
+              {_id}
+            </Text>
+          </View>
+        ))
+      }
+      <Button title="send data" onPress={() => { addNewActivity(givenActivities); }} />
+      <Button title="get all data" onPress={() => { getAllActivities(); }} />
     </View>
   );
 }
