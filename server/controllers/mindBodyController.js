718a91d9035d936a1968ab3aaeaf29b939747968
cherylwu834 <124432604+cherylwu834@users.noreply.github.com>
2023-11-30 22:36:29 -0800

    Mind and Body Post & GetRandom Backend Funcitons
    Added backend functions for creating a new mind and body moment and getting a random mind and body moment; Added controller functions specified above and set up the appropriate routes

---
+++ server/controllers/mindBodyController.js
@@ -0,0 +1,43 @@
+const MindBody = require('../models/mindBodyModel');
+
+// Example of creating a document in the database
+const createMoment = async (req, res) => {
+  const test = new MindBody(req.body);
+  try {
+    const data = await test.save(test);
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getRandomMoment = async (req, res) => {
+  try {
+    // Generate a random index based on the count
+    // Use aggregation to get a random document from the collection
+    const randomMindBody = await MindBody.aggregate([
+      { $sample: { size: 1 } }, // $sample stage to get a random document
+    ]);
+    // Extract the moment string from the random document
+    const randomMindBodyActivity = randomMindBody.length > 0 ? randomMindBody[0].activity : null;
+    const randomMindBodyDuration = randomMindBody.length > 0 ? randomMindBody[0].duration : null;
+    if (randomMindBodyActivity === null || randomMindBodyActivity === undefined) {
+      console.log('No valid Mind and Body Moment  found');
+      return 'No Mind and Body Moment here!';
+    }
+    if (randomMindBodyDuration === null || randomMindBodyDuration === undefined) {
+      console.log('No valid Mind and Body Moment Duration found');
+      return 'No Mind and Body Moment Duration here!';
+    }
+    res.send(randomMindBody[0]);
+  } catch (err) {
+    console.error(err);
+    // Handle the error appropriately
+    throw err;
+  }
+};
+
+module.exports = {
+  createMoment,
+  getRandomMoment,
+};
