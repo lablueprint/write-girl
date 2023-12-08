c596a9f74bcf4479f3d88052dbaafde8296ebbf3
ayubali24 <ayubali2443@gmail.com>
2023-12-07 22:50:15 -0800

    overlayed notification banner over App, plan to implement popup soon aka next quarter

--- mobile/App.jsx
+++ mobile/App.jsx
@@ -1,8 +1,47 @@
 import 'react-native-gesture-handler';
+import { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import AppNavigation from './Navigation/AppNavigation';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    zIndex: 1,
+    backgroundColor: 'red',
+    marginTop: 100,
+    position: 'absolute',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+const screenStyles = StyleSheet.create({
+  container: {
+    zIndex: 0,
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+});
+
 export default function App() {
+  const [notification, setNotification] = useState(null);
+  // Setup the notfications in top-level.
   return (
-    <AppNavigation />
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <Text>
+          Hello
+        </Text>
+      </View>
+      <View style={screenStyles.container}>
+        <AppNavigation />
+      </View>
+    </View>
   );
 }
