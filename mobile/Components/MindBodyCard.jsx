13d5b070b33520ca6157077e1d43ad937b7108dd
cherylwu834 <124432604+cherylwu834@users.noreply.github.com>
2024-01-15 21:32:32 -0800

    Deleted comment Co-authored-by: Joy Cheng <joycheng@ucla.edu>

--- mobile/Components/MindBodyCard.jsx
+++ mobile/Components/MindBodyCard.jsx
@@ -3,49 +3,9 @@ import {
   View, Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  card: {
-    flex: 1,
-    borderRadius: 4,
-    borderWidth: 2,
-    borderColor: '#E8E8E8',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 50,
-    backgroundColor: 'transparent',
-  },
-  done: {
-    textAlign: 'center',
-    fontSize: 30,
-    color: 'white',
-    backgroundColor: 'transparent',
-  },
-}); */
 
 export default function MindBodyCard({ activity, duration, boxWidth }) {
   return (
-    /*
-    <View style={styles.card}>
-      <Text>
-        Activity:
-        {' '}
-        {activity}
-      </Text>
-      <Text>
-        Duration:
-        {' '}
-        {duration}
-      </Text>
-    </View>
-    */
     <View
       style={{
         height: '100%',
