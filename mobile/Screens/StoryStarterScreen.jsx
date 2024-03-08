6a0ee6cec5f2a6f0c3b96ddc8ab23622ace19c49
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2024-03-07 20:45:25 -0800

    Update StoryStarterScreen.jsx

--- mobile/Screens/StoryStarterScreen.jsx
+++ mobile/Screens/StoryStarterScreen.jsx
@@ -3,7 +3,6 @@ import {
   StyleSheet, Text, View, Pressable, ImageBackground,
 } from 'react-native';
 import PropTypes from 'prop-types';
-// import axios from 'axios';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,13 +14,13 @@ const styles = StyleSheet.create({
   settingsContainer: {
     position: 'absolute',
     flex: 1,
-    top: 0,
+    top: 50,
     left: 0,
   },
   plotPointsContainer: {
     position: 'absolute',
     flex: 1,
-    top: 0,
+    top: -50,
     right: 0,
   },
   characterTraitsContainer: {
@@ -33,25 +32,24 @@ const styles = StyleSheet.create({
   objectsContainer: {
     position: 'absolute',
     flex: 1,
-    bottom: 0,
-    right: 0,
+    bottom: 40,
+    right: -20,
   },
   image: {
+    resizeMode: 'contain',
   },
   imageText: {
     color: 'white',
-    fontSize: 20, // Adjust as needed
+    fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 'auto',
     marginBottom: 'auto',
   },
   button: {
-    height: 200,
-    // width: 'auto',
-    resizeMode: 'contain',
+    height: 340,
+    width: 190,
     flex: 1,
-    aspectRatio: 0.9,
   },
 });
 
@@ -60,29 +58,25 @@ const objectsImage = require('../assets/story-starter-icons/objects.png');
 const plotPointsImage = require('../assets/story-starter-icons/plot-points.png');
 const settingsImage = require('../assets/story-starter-icons/settings.png');
 
-export default function StoryStarterScreen({ navigation, key }) {
-  // const [trait, setTrait] = useState('');
-  // const [plotPoint, setPlotPoint] = useState('');
-  // const [setting, setSetting] = useState('');
-  // const [object, setObject] = useState('');
+export default function StoryStarterScreen({ navigation }) {
   const navigateToSettingsScreen = () => {
-    navigation.navigate('Settings', {});
+    navigation.navigate('Settings');
   };
 
   const navigateToObjectsScreen = () => {
-    navigation.navigate('Objects', {});
+    navigation.navigate('Objects');
   };
 
   const navigateToTraitsScreen = () => {
-    navigation.navigate('Character Traits', {});
+    navigation.navigate('Character Traits');
   };
 
   const navigateToPlotPointsScreen = () => {
-    navigation.navigate('Plot Points', {});
+    navigation.navigate('Plot Points');
   };
 
   return (
-    <View style={styles.container} key={key}>
+    <View style={styles.container}>
       <Pressable onPress={navigateToSettingsScreen} title="Get random setting" style={styles.settingsContainer}>
         <ImageBackground source={settingsImage} style={styles.button} imageStyle={styles.image}>
           <Text style={styles.imageText}>Settings</Text>
@@ -108,7 +102,6 @@ export default function StoryStarterScreen({ navigation, key }) {
 }
 
 StoryStarterScreen.propTypes = {
-  key: PropTypes.number.isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func,
   }).isRequired,
