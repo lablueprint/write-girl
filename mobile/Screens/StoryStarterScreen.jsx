8ecdbb5ebb6384d1ebfa23478729e456c1ecc6d9
danielkao14 <daniel.kao196@gmail.com>
2024-04-11 23:11:23 -0700

    story starters mid fi 3 rough draft
    rough styling for mid fi 3 version of story starters

--- mobile/Screens/StoryStarterScreen.jsx
+++ mobile/Screens/StoryStarterScreen.jsx
@@ -7,33 +7,31 @@ import PropTypes from 'prop-types';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    display: 'flex',
-    flexDirection: 'column',
+    backgroundColor: '#151716',
   },
   settingsContainer: {
     position: 'absolute',
     flex: 1,
-    top: 50,
-    left: 0,
+    top: 240,
+    left: 200,
   },
   plotPointsContainer: {
     position: 'absolute',
     flex: 1,
-    top: -50,
-    right: 0,
+    top: 100,
+    right: 130,
   },
   characterTraitsContainer: {
     position: 'absolute',
     flex: 1,
-    bottom: 0,
-    left: 0,
+    bottom: -120,
+    left: 160,
   },
   objectsContainer: {
     position: 'absolute',
     flex: 1,
-    bottom: 40,
-    right: -20,
+    bottom: 20,
+    right: 210,
   },
   image: {
     resizeMode: 'contain',
@@ -51,12 +49,28 @@ const styles = StyleSheet.create({
     width: 190,
     flex: 1,
   },
+  titleText: {
+    color: 'white',
+    fontSize: 45, // Optional: Set the font size
+    fontWeight: 'bold', // Optional: Set the font weight
+  },
+  bodyText: {
+    color: 'white',
+    fontSize: 14, // Optional: Set the font size
+    fontWeight: 'bold', // Optional: Set the font weight
+  },
+  textContainer: {
+    margin: 5,
+    padding: 5,
+    flexDirection: 'column',
+  },
 });
 
-const traitsImage = require('../assets/story-starter-icons/character-traits.png');
-const objectsImage = require('../assets/story-starter-icons/objects.png');
-const plotPointsImage = require('../assets/story-starter-icons/plot-points.png');
-const settingsImage = require('../assets/story-starter-icons/settings.png');
+const traitsImage = require('../assets/story-starter-icons/character-traits-blob.png');
+const objectsImage = require('../assets/story-starter-icons/objects-blob.png');
+const plotPointsImage = require('../assets/story-starter-icons/plot-points-blob.png');
+const settingsImage = require('../assets/story-starter-icons/setting-blob.png');
+const backgroundImage = require('../assets/story-starter-icons/story-starters-background.png');
 
 export default function StoryStarterScreen({ navigation }) {
   const navigateToSettingsScreen = () => {
@@ -76,27 +90,34 @@ export default function StoryStarterScreen({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Pressable onPress={navigateToSettingsScreen} title="Get random setting" style={styles.settingsContainer}>
-        <ImageBackground source={settingsImage} style={styles.button} imageStyle={styles.image}>
-          <Text style={styles.imageText}>Settings</Text>
-        </ImageBackground>
-      </Pressable>
-      <Pressable onPress={navigateToTraitsScreen} title="Get random character trait" style={styles.characterTraitsContainer}>
-        <ImageBackground source={traitsImage} style={styles.button} imageStyle={styles.image}>
-          <Text style={styles.imageText}>Character Traits</Text>
-        </ImageBackground>
-      </Pressable>
-      <Pressable onPress={navigateToPlotPointsScreen} title="Get random plot point" style={styles.plotPointsContainer}>
-        <ImageBackground source={plotPointsImage} style={styles.button} imageStyle={styles.image}>
-          <Text style={styles.imageText}>Plot Points</Text>
-        </ImageBackground>
-      </Pressable>
-      <Pressable onPress={navigateToObjectsScreen} title="Get random object" style={styles.objectsContainer}>
-        <ImageBackground source={objectsImage} style={styles.button} imageStyle={styles.image}>
-          <Text style={styles.imageText}>Objects</Text>
-        </ImageBackground>
-      </Pressable>
+    <View>
+      <ImageBackground source={backgroundImage} style={{ width: '100%', height: '100%' }}>
+        <View style={styles.textContainer}>
+          <Text style={styles.titleText}> Story </Text>
+          <Text style={styles.titleText}> Starters </Text>
+          <Text style={styles.bodyText}> Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing. </Text>
+        </View>
+        <Pressable onPress={navigateToSettingsScreen} title="Get random setting" style={styles.settingsContainer}>
+          <ImageBackground source={settingsImage} style={styles.button} imageStyle={styles.image}>
+            <Text style={styles.imageText}>Settings</Text>
+          </ImageBackground>
+        </Pressable>
+        <Pressable onPress={navigateToTraitsScreen} title="Get random character trait" style={styles.characterTraitsContainer}>
+          <ImageBackground source={traitsImage} style={styles.button} imageStyle={styles.image}>
+            <Text style={styles.imageText}>Character Traits</Text>
+          </ImageBackground>
+        </Pressable>
+        <Pressable onPress={navigateToPlotPointsScreen} title="Get random plot point" style={styles.plotPointsContainer}>
+          <ImageBackground source={plotPointsImage} style={styles.button} imageStyle={styles.image}>
+            <Text style={styles.imageText}>Plot Points</Text>
+          </ImageBackground>
+        </Pressable>
+        <Pressable onPress={navigateToObjectsScreen} title="Get random object" style={styles.objectsContainer}>
+          <ImageBackground source={objectsImage} style={styles.button} imageStyle={styles.image}>
+            <Text style={styles.imageText}>Objects</Text>
+          </ImageBackground>
+        </Pressable>
+      </ImageBackground>
     </View>
   );
 }
