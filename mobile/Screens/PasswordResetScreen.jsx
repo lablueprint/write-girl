b794de95d75deb1729c076957a7e15e8d329cb60
Caroline DebBaruah <carolinedebbaruah@MacBook-Pro-15.local>
2024-05-20 03:19:15 -0700

    password reset hifis

--- mobile/Screens/PasswordResetScreen.jsx
+++ mobile/Screens/PasswordResetScreen.jsx
@@ -1,28 +1,87 @@
+/* eslint-disable max-len */
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import {
-  View, Text, StyleSheet, Button, TextInput,
+  View, Text, StyleSheet, Button, TextInput, ImageBackground, Image, TouchableOpacity, Pressable,
 } from 'react-native';
 import axios from 'axios';
+import emailIcon from '../assets/sign-up/emailIcon.png';
+import passwordIcon from '../assets/sign-up/passwordIcon.png';
+import welcomeBackground from '../assets/sign-up/signupbackground.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    paddingTop: 100,
-    padding: 40,
+  },
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+  },
+  paddingContainer: {
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  heading: {
+    marginTop: 120,
+    fontSize: 50,
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
+    fontWeight: 'bold',
+    lineHeight: 50,
+  },
+  headingNewPass: {
+    marginTop: 120,
+    fontSize: 50,
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
+    fontWeight: 'bold',
+    lineHeight: 50,
+    marginBottom: 50,
+  },
+  subheading: {
+    marginTop: 35,
+    marginBottom: 35,
+    fontSize: 20,
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
   },
   textfields: {
     fontSize: 16,
-    width: '100%',
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
   },
   inputContainer: {
-    backgroundColor: '#D9D9D9',
+    backgroundColor: '#ffffff33',
     flexDirection: 'row',
     alignItems: 'center',
-    borderRadius: 5,
-    marginTop: 10,
-    padding: 10,
+    borderRadius: 14,
+    padding: 14,
+  },
+  inputContainerCode: {
+    backgroundColor: '#ffffff33',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 14,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  inputContainerNewPass: {
+    backgroundColor: '#ffffff33',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 14,
+    padding: 14,
+    marginTop: 20,
+  },
+  textfieldsCode: {
+    fontSize: 70,
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
+    fontWeight: 'bold',
+  },
+  icon: {
+    marginRight: 14,
+    marginLeft: 10,
   },
   errorContainer: {
     backgroundColor: 'red',
@@ -30,6 +89,43 @@ const styles = StyleSheet.create({
   errorMsg: {
     color: 'white',
     fontSize: 16,
+  },
+  submitButton: {
+    backgroundColor: '#BFD25A',
+    borderRadius: 14,
+    marginTop: 60,
+    marginBottom: 10,
+    padding: 5,
+  },
+  button: {
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#214553',
+    fontSize: 18,
+    fontFamily: 'Helvetica Neue',
+    fontWeight: 'bold',
+  },
+  resetTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 5,
+  },
+  resetText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
+  },
+  resetRedirect: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'Helvetica Neue',
+    textDecorationLine: 'underline',
   },
 });
 
@@ -97,66 +193,103 @@ export default function PasswordResetScreen({ navigation }) {
     let view = [<View />];
     if (step === 'getVerification') {
       view = [
-        <View key="back1">
-          <Button title="Back" onPress={redirectLogIn} />
+        <View>
+          <View key="back1">
+            <Button title="Back" onPress={redirectLogIn} />
+          </View>
+          <Text style={styles.heading}>Forgot your password? </Text>
+          <Text style={styles.subheading}>No worries! Enter your email to receive a reset code </Text>
+          <View style={styles.inputContainer} key="email">
+            <Image source={emailIcon} style={styles.icon} />
+            <TextInput
+              style={styles.textfields}
+              onChangeText={handleChangeEmail}
+              value={email}
+              placeholder="Email"
+              placeholderTextColor="white"
+              autoCapitalize="none"
+            />
+          </View>
+          <View style={styles.submitButton}>
+            <TouchableOpacity onPress={sendPasswordReset} style={styles.button}>
+              <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
         </View>,
-        <View style={styles.inputContainer} key="email">
-          <TextInput
-            style={styles.textfields}
-            onChangeText={handleChangeEmail}
-            value={email}
-            placeholder="Email"
-            placeholderTextColor="#000000"
-            autoCapitalize="none"
-          />
-        </View>,
-        <Button key="submit" title="Submit" onPress={sendPasswordReset} />,
       ];
     } else if (step === 'verifyCode') {
       view = [
-        <View key="back2">
-          <Button title="Back" onPress={() => { setStep('getVerification'); }} />
+        <View>
+          <View key="back2">
+            <Button title="Back" onPress={() => { setStep('getVerification'); }} />
+          </View>
+          <Text style={styles.heading}>Forgot your password? </Text>
+          <Text style={styles.subheading}>
+            We've sent a code to 
+            {email}
+            . Enter the code you received in your email.
+          </Text>
+          <View style={styles.inputContainerCode} key="verify">
+            <TextInput
+              style={styles.textfieldsCode}
+              onChangeText={handleChangeCode}
+              placeholder="0 0 0 0"
+              placeholderTextColor="white"
+              value={code}
+            />
+          </View>
+          <View style={styles.submitButton}>
+            <TouchableOpacity onPress={verifyCode} style={styles.button}>
+              <Text style={styles.buttonText}>Enter</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.resetTextContainer}>
+            <Text style={styles.resetText}>
+              Didn’t receive the email?
+            </Text>
+            <Pressable onPress={sendPasswordReset}>
+              <Text style={styles.resetRedirect}> Click to resend</Text>
+            </Pressable>
+          </View>
         </View>,
-        <View style={styles.inputContainer} key="verify">
-          <TextInput
-            style={styles.textfields}
-            onChangeText={handleChangeCode}
-            placeholder="Verification Code"
-            placeholderTextColor="#000000"
-            value={code}
-          />
-        </View>,
-        <Button key="submit2" title="Get Another Code" onPress={sendPasswordReset} />,
-        <Button key="submit3" title="Check Code" onPress={verifyCode} />,
       ];
     } else if (step === 'inputNewPassword') {
       view = [
-        <View key="cancel">
-          <Button title="Cancel" onPress={redirectLogIn} />
+        <View>
+          <View key="cancel">
+            <Button title="Cancel" onPress={redirectLogIn} />
+          </View>
+          <Text style={styles.headingNewPass}>Password reset </Text>
+          <View style={styles.inputContainer} key="newpswd">
+            <Image source={passwordIcon} style={styles.icon} />
+            <TextInput
+              style={styles.textfields}
+              onChangeText={setNewPassword}
+              placeholder="New Password"
+              placeholderTextColor="white"
+              autoCapitalize="none"
+              value={newPassword}
+              secureTextEntry
+            />
+          </View>
+          <View style={styles.inputContainerNewPass} key="confirmpswd">
+            <Image source={passwordIcon} style={styles.icon} />
+            <TextInput
+              style={styles.textfields}
+              onChangeText={setConfirmation}
+              placeholder="Confirm New Password"
+              placeholderTextColor="white"
+              autoCapitalize="none"
+              value={confirmation}
+              secureTextEntry
+            />
+          </View>
+          <View style={styles.submitButton}>
+            <TouchableOpacity onPress={updatePassword} style={styles.button}>
+              <Text style={styles.buttonText}>Enter</Text>
+            </TouchableOpacity>
+          </View>
         </View>,
-        <View style={styles.inputContainer} key="newpswd">
-          <TextInput
-            style={styles.textfields}
-            onChangeText={setNewPassword}
-            placeholder="New Password"
-            placeholderTextColor="#000000"
-            autoCapitalize="none"
-            value={newPassword}
-            secureTextEntry
-          />
-        </View>,
-        <View style={styles.inputContainer} key="confirmpswd">
-          <TextInput
-            style={styles.textfields}
-            onChangeText={setConfirmation}
-            placeholder="Confirm New Password"
-            placeholderTextColor="#000000"
-            autoCapitalize="none"
-            value={confirmation}
-            secureTextEntry
-          />
-        </View>,
-        <Button key="submit3" title="Reset" onPress={updatePassword} />,
       ];
     }
     return view;
@@ -164,7 +297,12 @@ export default function PasswordResetScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {
+      <ImageBackground
+        source={welcomeBackground} // Replace with your image path
+        style={styles.imageBackground}
+      >
+        <View style={styles.paddingContainer}>
+          {
         errorMsg ? (
           <View style={styles.errorContainer}>
             <Text style={styles.errorMsg}>
@@ -175,7 +313,9 @@ export default function PasswordResetScreen({ navigation }) {
           <View />
         )
       }
-      {updateDisplay()}
+          {updateDisplay()}
+        </View>
+      </ImageBackground>
     </View>
   );
 }
