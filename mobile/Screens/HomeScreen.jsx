2fbf7f77a4335d6359be6ca1eb1942f1b507aedb
danielkao14 <daniel.kao196@gmail.com>
2024-06-03 23:01:12 -0700

    Basic styling
    Add pngs,
    style bottom size

--- mobile/Screens/HomeScreen.jsx
+++ mobile/Screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
-  StyleSheet, Text, View, Dimensions, ScrollView, ImageBackground,
+  StyleSheet, Text, View, Dimensions, TouchableOpacity, ImageBackground, Animated,
 } from 'react-native';
 import axios from 'axios';
 import PropTypes from 'prop-types';
@@ -12,7 +12,10 @@ import DoorActivityImage from '../assets/dooractivity.png';
 import TripleFlipsImage from '../assets/tripleflips.png';
 import HomeBackground from '../assets/home-screen.png';
 
-const window = Dimensions.get('window');
+import PostItsImage from '../assets/post_it.png';
+import FreeWriteImage from '../assets/free-write-card.png';
+import MindAndBodyImage from '../assets/mind-and-body-card.png';
+
 const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
@@ -41,6 +44,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     padding: '5%',
     paddingTop: 0,
+    paddingBottom: '5%',
   },
   greeting: {
     color: 'white',
@@ -48,10 +52,66 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     paddingTop: 20,
   },
+  needInspoText: {
+    color: '#BFD25A',
+  },
+  needInspoText2: {
+    color: 'white',
+    fontSize: 20,
+    paddingLeft: '5%',
+  },
   carouselContainer: {
     height: Dimensions.get('window').width / 1.5,
     marginTop: 10,
     marginBottom: '5%',
+  },
+  card: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    borderRadius: 20,
+    height: Dimensions.get('window').height / 4.5, // Adjust based on your image height
+    width: Dimensions.get('window').width,
+  },
+  cardText: {
+    fontSize: 18,
+    color: 'black',
+  },
+  postItContainer: {
+    flex: 1,
+    height: Dimensions.get('window').height,
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  PostItBackground: {
+    height: Dimensions.get('window').height / 2.9, // Adjust based on your image height
+    width: '100%',
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  invisibleButton: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderColor: 'red',
+    width: 175,
+    height: 100,
+  },
+  postIt1: {
+    top: 70, // Adjust based on the position of the first post-it note
+    left: 10,
+  },
+  postIt2: {
+    top: 180, // Adjust based on the position of the second post-it note
+    left: 80,
+  },
+  postIt3: {
+    top: 70, // Adjust based on the position of the third post-it note
+    left: 190,
   },
 });
 
@@ -82,11 +142,17 @@ const activityData = [
   },
 ];
 
+const exploreMoreData = [
+  { id: 1, text: 'Explore Card 1', background: FreeWriteImage },
+  { id: 2, text: 'Explore Card 2', background: MindAndBodyImage },
+];
+
 export default function HomeScreen({ navigation }) {
   const [pepTalks, setPepTalks] = useState('');
   const [writingTips, setWritingTips] = useState('');
   const [page, setPage] = React.useState('pep_talk');
   const [cardData, setCardData] = React.useState('default_text');
+  const scrollY = useRef(new Animated.Value(0)).current;
 
   // Retrieves and sets cardText to a PepTalk json object from database
   const getCardText = async (route) => {
@@ -100,13 +166,59 @@ export default function HomeScreen({ navigation }) {
       return err;
     }
   };
+  const handlePress = (screen) => {
+    navigation.navigate(screen);
+  };
 
   useEffect(() => {
     getCardText('/pepTalk/get');
   }, []);
 
+  const renderExploreMoreCards = () => exploreMoreData.map((item, index) => {
+    const inputRange = [-1, 0, (index) * 100, (index + 1) * 100];
+    const scale = scrollY.interpolate({
+      inputRange,
+      outputRange: [0, 0, 1, 1],
+      extrapolate: 'clamp',
+    });
+    const opacity = scrollY.interpolate({
+      inputRange,
+      outputRange: [0, 0, 0, 1],
+      extrapolate: 'clamp',
+    });
+    const translateY = scrollY.interpolate({
+      inputRange,
+      outputRange: [-30, -20, -30, -40],
+      extrapolate: 'clamp',
+    });
+
+    return (
+      <Animated.View
+        key={item.id}
+        style={[
+          // styles.card,
+          {
+            transform: [{ scale }, { translateY }],
+            opacity,
+          },
+        ]}
+      >
+        <ImageBackground source={item.background} style={styles.card}>
+          <Text style={styles.cardText}>{item.text}</Text>
+        </ImageBackground>
+      </Animated.View>
+    );
+  });
+
   return (
-    <ScrollView style={styles.all}>
+    <Animated.ScrollView
+      style={styles.all}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+        { useNativeDriver: true },
+      )}
+      scrollEventThrottle={16}
+    >
       <ImageBackground
         source={HomeBackground} // Replace with your image path
         style={styles.imageBackground}
@@ -141,9 +253,32 @@ export default function HomeScreen({ navigation }) {
           )}
         />
       </View>
-      <Text style={styles.title}>Explore More</Text>
-
-    </ScrollView>
+      <Text style={[styles.title, { paddingBottom: '10%' }]}>Explore More</Text>
+      {renderExploreMoreCards()}
+      <Text style={styles.title}>Need Inspo?</Text>
+      <Text style={styles.needInspoText2}>
+        <Text style={styles.needInspoText}>Tap on one </Text>
+        of these sticky notes
+      </Text>
+      <Text style={styles.needInspoText2}>to get a tip </Text>
+      <ImageBackground
+        source={PostItsImage}
+        style={styles.PostItBackground}
+      >
+        <TouchableOpacity
+          style={[styles.invisibleButton, styles.postIt1]}
+          onPress={() => handlePress('Writing Activities')}
+        />
+        <TouchableOpacity
+          style={[styles.invisibleButton, styles.postIt2]}
+          onPress={() => handlePress('Story Starter Stack')}
+        />
+        <TouchableOpacity
+          style={[styles.invisibleButton, styles.postIt3]}
+          onPress={() => handlePress('')}
+        />
+      </ImageBackground>
+    </Animated.ScrollView>
   );
 }
 
